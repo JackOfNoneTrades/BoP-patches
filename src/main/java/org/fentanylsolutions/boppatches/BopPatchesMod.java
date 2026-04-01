46845899086858387d15d813ca16392f6d726b73
jack <181404551+JackOfNoneTrades@users.noreply.github.com>
2026-04-01 14:53:50 +0200

    removed backgroundFile

--- src/main/java/org/fentanylsolutions/boppatches/BopPatchesMod.java
+++ src/main/java/org/fentanylsolutions/boppatches/BopPatchesMod.java
@@ -16,8 +16,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
     dependencies = "required-after:BiomesOPlenty",
     customProperties = { @Mod.CustomProperty(k = "license", v = "LGPLv3+SNEED"),
         @Mod.CustomProperty(k = "issueTrackerUrl", v = "https://github.com/JackOfNoneTrades/BoP-patches/issues"),
-        @Mod.CustomProperty(k = "iconFile", v = "assets/boppatches/logo.png"),
-        @Mod.CustomProperty(k = "backgroundFile", v = "assets/boppatches/background.png") },
+        @Mod.CustomProperty(k = "iconFile", v = "assets/boppatches/logo.png") },
     acceptedMinecraftVersions = "[1.7.10]")
 public class BopPatchesMod {
 
